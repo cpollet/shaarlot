2fa4d2edc2c0df69d0b81bf84c559c9237c40060
Christophe Pollet <cpollet@users.noreply.github.com>
2023-02-13 20:16:42 +0100

    Run migration when server starts

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use backend::database;
 use backend::rest::router;
+use sea_orm_migration::MigratorTrait;
 
 #[tokio::main]
 async fn main() {
     let database = database::connect().await;
+
+    migration::Migrator::up(&database, None)
+        .await
+        .expect("Could not migrate database");
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(router(database).into_make_service())
