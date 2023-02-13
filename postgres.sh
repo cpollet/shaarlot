edd6766bc59856b28a035d1f79f070ace9843e8c
Christophe Pollet <cpollet@users.noreply.github.com>
2023-02-13 19:40:17 +0100

    Fix postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-docker run -d \
-  --name rbm-postgres \
-  -e POSTGRES_PASSWORD=password \
-  -v rbm-postgres:/var/lib/postgresql/data \
-  -p 5432:5432 \
-  postgres
+if docker ps -a | grep rbm-postgres; then
+  docker start rbm-postgres
+else
+  docker run -d \
+    --name rbm-postgres \
+    -e POSTGRES_PASSWORD=password \
+    -v rbm-postgres:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    postgres
+fi
