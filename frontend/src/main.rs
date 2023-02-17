a6e752c93e8c5f1495ab0fe5fa7bfbb686884eda
Christophe Pollet <cpollet@users.noreply.github.com>
2023-02-17 20:19:08 +0100

    Implement menu and router

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -3,22 +3,73 @@ mod bookmarks;
 mod bookmarks_provider;
 mod qr_code;
 mod qr_code_overlay;
+mod menu;
 
 use crate::bookmarks::BookmarksHOC;
 use crate::bookmarks_provider::BookmarksProvider;
+use crate::menu::Menu;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 fn main() {
     yew::Renderer::<App>::new().render();
 }
 
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Index,
+    #[at("/bookmarks")]
+    Bookmarks,
+    #[at("/bookmarks/new")]
+    AddBookmark,
+    #[at("/tags")]
+    TagCloud,
+    #[at("/tools")]
+    Tools,
+    #[not_found]
+    #[at("/404")]
+    NotFound
+}
+
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <div class="view">
+        <BrowserRouter>
+            <Menu />
+            <div class="content">
+                <Switch<Route> render={switch} />
+            </div>
+        </BrowserRouter>
+    }
+}
+
+fn switch(route:Route) ->Html {
+    match route {
+        Route::Index | Route::Bookmarks => html! {
             <BookmarksProvider>
                 <BookmarksHOC />
             </BookmarksProvider>
-        </div>
+        },
+        Route::AddBookmark => {
+            html! {
+                {"todo: add bookmark"}
+            }
+        },
+        Route::TagCloud => {
+            html! {
+                {"todo: tag cloud"}
+            }
+        },
+        Route::Tools => {
+            html! {
+                {"todo: tools"}
+            }
+        },
+        Route::NotFound => {
+            html! {
+                <h1>{"404 Not Found"}</h1>
+            }
+        },
     }
 }
---
+++ frontend/src/menu.rs
@@ -0,0 +1,39 @@
+use yew::prelude::*;
+use yew_router::hooks::use_navigator;
+use yew_router::Routable;
+use crate::Route;
+
+#[function_component(Menu)]
+pub fn menu() -> Html {
+    let navigator = use_navigator().unwrap();
+
+    html! {
+        <div class="menu">
+            <a onclick={{let navigator = navigator.clone(); move |e:MouseEvent| {
+                navigator.push(&Route::Bookmarks);
+                e.prevent_default();
+            }}} class="menu__item" href={Route::Bookmarks.to_path()}>{"Bookmarks"}</a>
+
+            {"·"}
+
+            <a onclick={{let navigator = navigator.clone(); move |e:MouseEvent| {
+                navigator.push(&Route::TagCloud);
+                e.prevent_default();
+            }}} class="menu__item" href={Route::TagCloud.to_path()}>{"Tag cloud"}</a>
+
+            {"·"}
+
+            <a onclick={{let navigator = navigator.clone(); move |e:MouseEvent| {
+                navigator.push(&Route::AddBookmark);
+                e.prevent_default();
+            }}} class="menu__item" href={Route::AddBookmark.to_path()}>{"Add"}</a>
+
+            {"·"}
+
+            <a onclick={{let navigator = navigator.clone(); move |e:MouseEvent| {
+                navigator.push(&Route::Tools);
+                e.prevent_default();
+            }}} class="menu__item" href={Route::Tools.to_path()}>{"Tools"}</a>
+        </div>
+    }
+}
