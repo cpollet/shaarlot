8d5dfc5d60fa28b9868fd7eae5057cdfe4c6f83b
Christophe Pollet <cpollet@users.noreply.github.com>
2023-02-19 20:03:55 +0100

    Implement sorting on bookmarks page

--- frontend/src/bookmarks.rs
+++ frontend/src/bookmarks.rs
@@ -1,4 +1,5 @@
 use crate::bookmarks::bookmark::*;
+use crate::bookmarks::bookmarks_provider::Order;
 use crate::data::Bookmark as BookmarkData;
 use std::rc::Rc;
 use yew::prelude::*;
@@ -11,18 +12,64 @@ mod qr_code_overlay;
 #[derive(Properties, Clone, PartialEq)]
 pub struct Props {
     pub bookmarks: Rc<Vec<BookmarkData>>,
+    pub order: Order,
+    pub on_change_order: Callback<Order>,
+}
+
+struct State {
+    order: Order,
+}
+
+impl State {
+    fn from(props: &Props) -> Self {
+        Self {
+            order: props.order.clone(),
+        }
+    }
+}
+
+impl Order {
+    fn invert(&self) -> Self {
+        match self {
+            Order::Asc => Order::Desc,
+            Order::Desc => Order::Asc,
+        }
+    }
+    fn icon(&self) -> &str {
+        match self {
+            Order::Asc => "expand_less",
+            Order::Desc => "expand_more",
+        }
+    }
 }
 
 #[function_component(Bookmarks)]
 pub fn bookmarks(props: &Props) -> Html {
+    let state = use_state(|| State::from(props));
+
+    let onclick = {
+        let state = state.clone();
+        let props = props.clone();
+        Callback::from(move |e: MouseEvent| {
+            e.prevent_default();
+            props.on_change_order.emit(state.order.invert())
+        })
+    };
+
     html! {
-        <ul class="bookmarks">
-        {
-            props.bookmarks.as_slice().into_iter().map(|b| html! {
-                <Bookmark key={b.id} bookmark={b.clone()} />
-            }).collect::<Html>()
-        }
-        </ul>
+        <div>
+            <div class="bookmarks-header">
+                {"sort:"}
+                <a {onclick} class="material-icons-outlined md-18" href="#" >{state.order.icon()}</a>
+            </div>
+            <ul class="bookmarks">
+            {
+                props.bookmarks.as_slice().into_iter().map(|b| html! {
+                    <Bookmark key={b.id} bookmark={b.clone()} />
+                }).collect::<Html>()
+            }
+            </ul>
+        </div>
     }
 }
 
--- frontend/src/bookmarks/bookmarks_provider.rs
+++ frontend/src/bookmarks/bookmarks_provider.rs
@@ -1,4 +1,4 @@
-use crate::bookmarks::Props as BookmarksProps;
+use crate::bookmarks::Props as BookmarksContext;
 use crate::data::Bookmark;
 use gloo_net::http::Request;
 use rest_api::{BookmarkResponse, URL_BOOKMARKS};
@@ -11,45 +11,64 @@ pub struct Props {
     pub children: Children,
 }
 
+#[derive(Clone, PartialEq)]
+pub enum Order {
+    Asc,
+    Desc,
+}
+
+impl Order {
+    fn query_param(&self) -> &str {
+        match self {
+            Order::Asc => "asc",
+            Order::Desc => "desc",
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct State {
+    order: Order,
+    context: Option<BookmarksContext>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            order: Order::Desc,
+            context: None,
+        }
+    }
+}
+
 #[function_component(BookmarksProvider)]
 pub fn bookmarks_provider(props: &Props) -> Html {
-    let bookmarks = use_state(|| None);
+    let state = use_state(|| State::default());
+
+    let on_change_order = {
+        let state = state.clone();
+        Callback::from(move |order: Order| {
+            state.set(State {
+                order,
+                context: None,
+            });
+        })
+    };
 
     {
-        let bookmarks = bookmarks.clone();
+        let state = state.clone();
         use_effect(move || {
-            if bookmarks.is_none() {
+            if state.context.is_none() {
+                let state = state.clone();
                 spawn_local(async move {
-                    let v = match Request::get(URL_BOOKMARKS)
-                        .query([("order", "creation_date:desc")])
-                        .send()
-                        .await
-                    {
-                        Err(_) => Err("Error fetching data".to_string()),
-                        Ok(resp) => {
-                            if !resp.ok() {
-                                Err(format!(
-                                    "Error fetching data: {} ({})",
-                                    resp.status(),
-                                    resp.status_text()
-                                ))
-                            } else {
-                                resp.json::<Vec<BookmarkResponse>>()
-                                    .await
-                                    .map_err(|err| err.to_string())
-                                    .map(|elements| {
-                                        elements
-                                            .into_iter()
-                                            .map(Bookmark::from)
-                                            .collect::<Vec<Bookmark>>()
-                                    })
-                                    .map(|bookmarks| BookmarksProps {
-                                        bookmarks: Rc::new(bookmarks),
-                                    })
-                            }
-                        }
-                    };
-                    bookmarks.set(Some(v))
+                    let res = fetch_bookmarks(&state.order, on_change_order).await;
+                    // todo implement a 500 page
+                    if let Ok(bookmarks) = res {
+                        state.set(State {
+                            order: state.order.clone(),
+                            context: Some(bookmarks),
+                        })
+                    }
                 });
             }
 
@@ -57,17 +76,51 @@ pub fn bookmarks_provider(props: &Props) -> Html {
         });
     }
 
-    match bookmarks.as_ref() {
-        Some(Ok(bookmarks)) => html! {
-            <ContextProvider<BookmarksProps> context={(*bookmarks).clone()}>
+    match state.context.as_ref() {
+        Some(bookmarks) => html! {
+            <ContextProvider<BookmarksContext> context={(*bookmarks).clone()}>
                 { props.children.clone() }
-            </ContextProvider<BookmarksProps >>
-        },
-        Some(Err(err)) => html! {
-            <div>{err}</div>
+            </ContextProvider<BookmarksContext >>
         },
         None => html! {
-            <div>{"No data"}</div>
+            <div></div>
         },
+    }
+}
+
+async fn fetch_bookmarks(
+    order: &Order,
+    callback: Callback<Order>,
+) -> Result<BookmarksContext, String> {
+    match Request::get(URL_BOOKMARKS)
+        .query([("order", format!("creation_date:{}", order.query_param()))])
+        .send()
+        .await
+    {
+        Err(_) => Err("Error fetching data".to_string()),
+        Ok(resp) => {
+            if !resp.ok() {
+                Err(format!(
+                    "Error fetching data: {} ({})",
+                    resp.status(),
+                    resp.status_text()
+                ))
+            } else {
+                resp.json::<Vec<BookmarkResponse>>()
+                    .await
+                    .map_err(|err| err.to_string())
+                    .map(|elements| {
+                        elements
+                            .into_iter()
+                            .map(Bookmark::from)
+                            .collect::<Vec<Bookmark>>()
+                    })
+                    .map(|bookmarks| BookmarksContext {
+                        bookmarks: Rc::new(bookmarks),
+                        order: order.clone(),
+                        on_change_order: callback,
+                    })
+            }
+        }
     }
 }
